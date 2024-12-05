7d9b3bd32ae5fec654b3801b1655c8c61b1cc3de
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2024-12-05 20:26:38 +0000

    allow script to pass even if a line failed

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
-set -e
-
+ 
 # Colors for output
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
@@ -13,50 +11,50 @@ log_info() {
 main() {
     # Check if sudo is installed
     if ! command -v sudo &> /dev/null; then
-        apt update
-        apt install sudo -y
+        apt update || true
+        apt install sudo -y || true
     fi
 
     log_info "Updating apt..."
-    sudo apt update
+    sudo apt update || true
 
     log_info "Installing cmake python3-dev..."
-    sudo apt install python3-dev cmake -y
+    sudo apt install python3-dev cmake -y || true
 
     log_info "Installing iperf..."
-    sudo apt install iperf -y
+    sudo apt install iperf -y || true
 
     log_info "Cloning repository..."
-    git clone https://github.com/PrimeIntellect-ai/prime.git
+    git clone https://github.com/PrimeIntellect-ai/prime.git || true
     
     log_info "Entering project directory..."
-    cd prime
+    cd prime || true
     
     log_info "Installing uv..."
-    curl -LsSf https://astral.sh/uv/install.sh | sh
+    curl -LsSf https://astral.sh/uv/install.sh | sh || true
     
     log_info "Sourcing uv environment..."
-    source $HOME/.local/bin/env
+    source $HOME/.local/bin/env || true
         
     log_info "Creating virtual environment..."
-    uv venv
+    uv venv || true
     
     log_info "Activating virtual environment..."
-    source .venv/bin/activate
+    source .venv/bin/activate || true
     
     log_info "Installing dependencies..."
-    uv sync --extra all
+    uv sync --extra all || true
     
     log_info "Installing flash-attn..."
-    uv pip install flash-attn==2.6.3 --no-build-isolation
+    uv pip install flash-attn==2.6.3 --no-build-isolation || true
     
     log_info "Updating git submodules..."
-    git submodule update --init --recursive
+    git submodule update --init --recursive || true
     
     log_info "Downloading data..."
-    mkdir -p datasets
-    uv run python scripts/subset_data.py --dataset_name PrimeIntellect/fineweb-edu --data_world_size 1 --data_rank 0 --max_shards 128
-    mv fineweb-edu/ datasets/fineweb-edu/
+    mkdir -p datasets || true
+    uv run python scripts/subset_data.py --dataset_name PrimeIntellect/fineweb-edu --data_world_size 1 --data_rank 0 --max_shards 128 || true
+    mv fineweb-edu/ datasets/fineweb-edu/ || true
 
     log_info "Installation completed! You can double check that everything is install correctly by running 'GLOO_SOCKET_IFNAME=lo GLOBAL_ADDR=localhost GLOBAL_RANK=0 GLOBAL_UNIQUE_ID=0 GLOBAL_WORLD_SIZE=1 GLOBAL_PORT=8989  uv run torchrun --nproc_per_node=2 src/zeroband/train.py  @configs/debug/diloco.toml'"
 }

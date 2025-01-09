650183e8d0cb55a043201f42e7280356d6e39cf1
sami jaghouar <sami.jaghouar@gmail.com>
2025-01-09 12:43:25 +0000

    add soap optimizer

--- src/zeroband/optimizers.py
+++ src/zeroband/optimizers.py
@@ -1,7 +1,6 @@
-from typing import TypeAlias
+from typing import Literal, TypeAlias
 from pydantic_config import BaseConfig
 import torch
-from zeroband.optimizers.muon import Muon, AdamConfig, MuonConfig
 from distributed_shampoo import (
     DefaultEigenvalueCorrectedShampooConfig,
     DistributedShampoo,
@@ -9,8 +8,15 @@ from distributed_shampoo import (
     ShampooPT2CompileConfig,
 )
 
+class AdamConfig(BaseConfig):
+    type: Literal["adam"] = "adam" # the literal is used to distinguish between the different optimizers configuration in the union type
+    lr: float = 4e-4
+    weight_decay: float = 0.1
+    betas1: float = 0.9
+    betas2: float = 0.95
 
 class SoapConfig(BaseConfig):
+    type: Literal["soap"] = "soap"
     lr: float = 4e-4
     weight_decay: float = 1e-05
     betas1: float = 0.9
@@ -20,7 +26,7 @@ class SoapConfig(BaseConfig):
     precondition_frequency: int = 100
 
 
-OptimizersConfig: TypeAlias = AdamConfig | MuonConfig | SoapConfig
+OptimizersConfig: TypeAlias = AdamConfig | SoapConfig
 
 
 def get_optimizer(params: list[torch.nn.Parameter], config: OptimizersConfig) -> torch.optim.Optimizer:
@@ -30,17 +36,6 @@ def get_optimizer(params: list[torch.nn.Parameter], config: OptimizersConfig) ->
             lr=config.lr,
             weight_decay=config.weight_decay,
             betas=(config.betas1, config.betas2),
-        )
-    elif isinstance(config, MuonConfig):
-        return Muon(
-            params,
-            lr=config.lr,
-            momentum=config.momentum,
-            nesterov=config.nesterov,
-            ns_steps=config.pseudo_order_steps,
-            adamw_lr=config.adam.lr,
-            adamw_betas=(config.adam.betas1, config.adam.betas2),
-            adamw_wd=config.adam.weight_decay,
         )
     elif isinstance(config, SoapConfig):
         return DistributedShampoo(

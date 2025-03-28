3134287478efac514e6fe423e336221d02109c9c
mike <michael.keiblinger@gmail.com>
2025-03-28 23:15:57 +0000

    accept hw config in apply_sharding

--- src/zeroband/utils/sharding_utils.py
+++ src/zeroband/utils/sharding_utils.py
@@ -16,7 +16,7 @@ def apply_sharding(hardware_config: HardwareConfig, model: torch.nn.Module):
     offload_policy = CPUOffloadPolicy(pin_memory=True) if hardware_config.fsdp_cpu_offload else None
 
     for layer_id, transformer_block in model.layers.items():
-        if config.hardware.reshard_after_forward:
+        if hardware_config.reshard_after_forward:
             # As an optimization, do not re-shard after forward for the last
             # transformer block since FSDP would prefetch it immediately
             reshard_after_forward = int(layer_id) < len(model.layers) - 1

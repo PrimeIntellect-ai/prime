e533f4a909102e5ff7851eef6fbe1d762f3bd86d
mike <michael.keiblinger@gmail.com>
2025-03-28 23:15:03 +0000

    accept hw config in apply_sharding

--- src/zeroband/utils/sharding_utils.py
+++ src/zeroband/utils/sharding_utils.py
@@ -1,19 +1,19 @@
 import torch.nn
 from torch.distributed.fsdp import fully_shard, MixedPrecisionPolicy, CPUOffloadPolicy
 
-from zeroband.config import Config
+from zeroband.config import HardwareConfig
 
 
-def apply_sharding(config: Config, model: torch.nn.Module):
+def apply_sharding(hardware_config: HardwareConfig, model: torch.nn.Module):
     """
     Applies the sharding strategy to the model according to the configuration.
     Will use FSDP with optional re-sharding for backward depending on configuration
     """
     mp_policy = MixedPrecisionPolicy(
-        param_dtype=torch.bfloat16, reduce_dtype=torch.float32 if config.hardware.reduce_fp32 else None
+        param_dtype=torch.bfloat16, reduce_dtype=torch.float32 if hardware_config.reduce_fp32 else None
     )
 
-    offload_policy = CPUOffloadPolicy(pin_memory=True) if config.hardware.fsdp_cpu_offload else None
+    offload_policy = CPUOffloadPolicy(pin_memory=True) if hardware_config.fsdp_cpu_offload else None
 
     for layer_id, transformer_block in model.layers.items():
         if config.hardware.reshard_after_forward:
@@ -32,6 +32,6 @@ def apply_sharding(config: Config, model: torch.nn.Module):
     fully_shard(
         model,
         mp_policy=mp_policy,
-        reshard_after_forward=config.hardware.reshard_after_forward,
+        reshard_after_forward=hardware_config.reshard_after_forward,
         offload_policy=offload_policy,
     )

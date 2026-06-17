b746f2d853c706be51b4a4fa94f0428d5339eb57
Cooper Miller <44559144+kcoopermiller@users.noreply.github.com>
2026-06-16 17:00:33 -0700

    release: tunnel 0.1.10 (#747)

--- packages/prime-tunnel/src/prime_tunnel/__init__.py
+++ packages/prime-tunnel/src/prime_tunnel/__init__.py
@@ -1,6 +1,6 @@
 """Prime Tunnel SDK - Expose local services via secure tunnels."""
 
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 
 from prime_tunnel.core import Config, TunnelClient
 from prime_tunnel.exceptions import (

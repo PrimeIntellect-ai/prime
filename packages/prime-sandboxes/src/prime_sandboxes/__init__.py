c6433af126d0e9d99431fad34d313b0d269f8289
hallerite <git@hallerite.com>
2026-08-05 04:18:34 +0200

    release: prime-sandboxes 0.2.35

--- packages/prime-sandboxes/src/prime_sandboxes/__init__.py
+++ packages/prime-sandboxes/src/prime_sandboxes/__init__.py
@@ -70,7 +70,7 @@ from .models import (
 from .process import AsyncSandboxProcess
 from .sandbox import AsyncSandboxClient, AsyncTemplateClient, SandboxClient, TemplateClient
 
-__version__ = "0.2.34"
+__version__ = "0.2.35"
 
 # Deprecated alias for backward compatibility
 TimeoutError = APITimeoutError

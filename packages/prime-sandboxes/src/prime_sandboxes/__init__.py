bf0e14976bf39826c8a661484fbbac3828610bc3
Andrew Kirillov <andrew@kirillov.org>
2026-09-07 14:27:26 -0700

    release: prime-sandboxes 0.2.42

--- packages/prime-sandboxes/src/prime_sandboxes/__init__.py
+++ packages/prime-sandboxes/src/prime_sandboxes/__init__.py
@@ -80,7 +80,7 @@ from .models import (
 from .process import AsyncSandboxProcess
 from .sandbox import AsyncSandboxClient, AsyncTemplateClient, SandboxClient, TemplateClient
 
-__version__ = "0.2.41"
+__version__ = "0.2.42"
 
 # Deprecated alias for backward compatibility
 TimeoutError = APITimeoutError
